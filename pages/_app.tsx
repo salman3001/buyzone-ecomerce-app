17f8ba3dd0fb698489c27da2978ac9dc8113239b
salman <therodfighter@gmail.com>
2022-12-10 11:09:10 +0400

    added storeProvider wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { StoreProvide } from '../context/store'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <StoreProvide>
+    <Component {...pageProps} />
+  </StoreProvide>)
 }
